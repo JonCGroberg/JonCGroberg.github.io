16155696e0c530a2d2a16c52e7f732f21aa6c748
Jonathan <jongroberg@gmail.com>
2024-07-20 08:50:02 -0400

    netlify testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import react from "@astrojs/react";
 import netlify from "@astrojs/netlify";
 
 export default defineConfig({
+  site: "https://joncgroberg.github.io",
   integrations: [mdx(), sitemap(), tailwind(), react()],
   output: "server",
   adapter: netlify({ edgeMiddleware: true }),
