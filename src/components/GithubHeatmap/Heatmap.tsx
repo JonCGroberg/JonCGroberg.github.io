0e1a448d59c972554034a66b2bf37dacc490921b
Jonathan <jongroberg@gmail.com>
2024-07-21 19:41:31 -0400

    hovering added and submenu

--- src/components/GithubHeatmap/Heatmap.tsx
+++ src/components/GithubHeatmap/Heatmap.tsx
@@ -1,31 +1,56 @@
-import type { Contribution, Year } from "../../types/heatmap";
+import { useState } from "react";
+import type { Contribution } from "../../types/heatmap";
 
 const shades = ["#ebedf0", "#9be9a8", "#40c463", "#30a14e", "#216e39"];
 interface HeatmapProps {
   contributions: Contribution[];
-  // years: Year[];
 }
 
 export default function Heatmap({ contributions }: HeatmapProps) {
+  const [selectedDay, setSelectedDay] = useState<Contribution | null>(null);
+  const [startYear, startMonth, startDay] = contributions[0].date.split("-");
+  const [endYear, endMonth, endDay] =
+    contributions[contributions.length - 1].date.split("-");
+  const totalContributions = contributions.reduce(
+    (acc, contribution) => Number(acc) + Number(contribution.intensity) * 2,
+    0,
+  );
+
   return (
     <div className=" flex-wrap">
-      <div className="grid grid-rows-7 grid-flow-col gap-[0px]">
+      {/* Sub Header */}
+      <div className="flex justify-between ">
+        <h2 className="text-xl font-bold hidden sm:block py-0 my-0">{` ${startYear} -  ${endYear}`}</h2>
+        <span className="sm:text-sm sm:text-gray-500 sm:font-normal text-base font-bold text mb-2 sm:mb-3  ">
+          {selectedDay ? (
+            <>
+              <span className="underline font-bold">
+                {selectedDay.intensity}
+              </span>{" "}
+              <span>Contributions on {selectedDay.date}</span>
+            </>
+          ) : (
+            <>
+              <span className="underline font-bold">{totalContributions}</span>{" "}
+              <span> Contributions in 12 months</span>
+            </>
+          )}
+        </span>
+      </div>
+      <div className="grid grid-rows-7 grid-flow-col gap-[0px] overflow-y-hidden -m-1 p-1">
         {/* Contribution Square Grid */}
         {contributions.map((contribution) => (
-          <div key={contribution.date} className="relative *:hover:flex group">
+          <div
+            key={contribution.date}
+            className="relative *:hover:flex group cursor-pointer"
+            onMouseOver={() => setSelectedDay(contribution)}
+            onMouseOut={() => setSelectedDay(null)}
+          >
             {/* Square */}
             <div
               className="p-[5px] rounded group-hover:outline group-hover:outline-2 group-hover:outline-green-400 group-hover:shadow m-[1px]"
               style={{ backgroundColor: shades[contribution.intensity] }}
             ></div>
-            {/* Toolhip Hover Message */}
-            <div className="absolute bottom-0 left-0 hidden bg-white px-3 mb-[15px] text-sm rounded-sm shadow outline outline-gray-200 z-10 flex-nowrap text-nowrap">
-              {contribution.intensity == 1
-                ? `${contribution.intensity} Contribution on ${contribution.date}`
-                : `${contribution.intensity} Contributions on ${contribution.date}`}
-              <br />
-              {getDayOfWeek(contribution.date)}
-            </div>
           </div>
         ))}
       </div>
@@ -44,4 +69,22 @@ function getDayOfWeek(date: string): string {
     "Saturday",
     "Sunday",
   ][dayOfWeek];
+}
+
+function getMonth(date: string): string {
+  const month = new Date(date).getMonth(); // 0-11
+  return [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ][month];
 }
