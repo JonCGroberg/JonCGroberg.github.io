731e78121e3efe6ae8abbf53e1ac5512e1c31039
Jonathan <jongroberg@gmail.com>
2024-07-25 17:57:58 -0400

    added google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,19 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import netlify from "@astrojs/netlify";
+import partytown from "@astrojs/partytown";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://joncgroberg.github.io",
-  integrations: [mdx(), sitemap(), tailwind(), react()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    react(),
+    partytown({ config: { forward: ["dataLayer.push"] } }),
+  ],
   output: "server",
   adapter: netlify(),
 });
